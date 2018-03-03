a466e8ad32c4eb4a26592f51355612be5cbb7808
Touhid Mia <hmtm.cse@gmail.com>
2018-03-03 07:04:53 +0600

    bismillah

---
+++ plugins/bismillah/bismillah.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Bismillah Plugin
+Plugin URI: http://www.hmtmcse.com/wp/plugins/bismillah
+Description: First Plugin of Wordpress by Touhid
+Author: Touhid Mia
+Version: 0.1
+Author URI: http://www.hmtmcse.com
+*/
